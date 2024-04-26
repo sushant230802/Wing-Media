a87b5ea294fa0a2a9628a79f55ea33b039cef70e
sushant230802 <sushantmalik2300@gmail.com>
2024-04-27 01:40:57 +0530

    feat:likes,dislikes,delete posts

--- backend/controllers/post..js
+++ backend/controllers/post..js
@@ -12,7 +12,7 @@ try{
             public_id:"req.body.public_id",
             url:"req.body.url",
         },
-        owner:req.user_id,
+        owner:req.user._id,
     }
 
     const post= await Post.create(newPostData);
@@ -33,4 +33,80 @@ try{
         message:error.message
     })
 }
+}
+
+exports.likeAndUnlikePost= async (req,res)=>{
+    try{
+        const post =await Post.findById(req.params.id);
+
+        if(!post){
+            return res.status(404),json({
+                success:"false",
+                message:"post not found",
+            })
+        }
+        if(post.likes.includes(req.user._id)){
+            const index=post.likes.indexOf(req.user_id);
+            post.likes.splice(index,1)  //it will unlike the post (liked already)
+            await post.save();
+            return res.status(200).json({
+                success:"true",
+                message:"post unliked",
+            })
+        }
+
+        post.likes.push(req.user._id);
+        await post.save();
+        return res.status(200).json({
+            success:"true",
+            message:"post liked",
+        })
+    }
+
+    catch(error){
+        res.status(500).json({
+            success:"false",
+            message:error.message,
+        })
+
+    }
+}
+
+exports.deletePost=async (req,res)=>{
+    try{
+        const post= await Post.findById(req.params.id);
+        if(!post){
+            return res.status(404),json({
+                success:"false",
+                message:"post not found",
+            })
+        }
+        if(post.owner.toString() != req.user._id.toString()){
+            return res.status(401).json({
+                success:"false",
+                message:"unauthorised"
+            })
+
+        }
+        await post.deleteOne(); //remove() func is depriciated in new version of mongoose
+
+        const user=await User.findById(req.user._id);
+
+        const index=user.posts.indexOf(req.params.id);
+
+        user.posts.splice(index,1);
+
+        await user.save();
+
+        res.status(200).json({
+            success:"true",
+            message:"post deleted",
+        })
+    }
+    catch(error){
+        res.status(500).json({
+            success:"false",
+            message:error.message,
+        })
+    }
 }
